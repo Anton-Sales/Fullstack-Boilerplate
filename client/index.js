27b75d09fcc4d78babbdce1107e8c35930e97890
anton-sales <anton.arus@gmail.com>
2018-08-07 21:15:35 +1200

    redux is wired up

--- client/index.js
+++ client/index.js
@@ -1,12 +1,19 @@
-console.log('hello client')
-
 import React from 'react'
-
 import { render } from 'react-dom'
+
+import App from './components/App'
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers/index'
+
+const store = createStore(reducers)
 
 document.addEventListener('DOMContentLoaded', () => {
     render(
-        <h1>hello world</h1>,
+        <Provider store={store}>
+            <App />
+        </Provider>,
         document.getElementById('app')
     )
 })
