61629fd6e21f88cc2f04a9a59d5838d222ec7279
anton-sales <anton.arus@gmail.com>
2018-09-25 16:07:44 +1200

    blank

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
         return(
             <div>
                 <h1>Hello user</h1>
-                <Users />
+                <Users /> 
             </div>
         )
     }
